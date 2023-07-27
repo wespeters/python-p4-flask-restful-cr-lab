e4f87f7d0a0da06576bd8162f5aa269ce4e91314
Wesley Peters <wesleypeters@gmail.com>
2023-07-27 11:26:21 -0600

    build plant model and routes

--- server/app.py
+++ server/app.py
@@ -17,11 +17,26 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return make_response(jsonify([plant.to_dict() for plant in plants]), 200)
+    
+    def post(self):
+        new_plant = Plant(**request.get_json())
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(jsonify(new_plant.to_dict()), 201)
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, plant_id):
+        plant = Plant.query.filter_by(id=plant_id).one_or_none()
+        if plant is None:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        else:
+            return make_response(jsonify(plant.to_dict()), 200)
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:plant_id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
